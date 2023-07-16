a30527d5b1a8c7915b3c73a360d0a610538ac34f
marina rodina <mrodina1990@yandex.ru>
2023-07-16 20:30:09 +0500

    install i18next and implement the output of interface texts through it

--- src/application.js
+++ src/application.js
@@ -1,35 +1,66 @@
 import * as yup from 'yup';
 import onChange from 'on-change';
+import i18next from 'i18next';
+
+import ru from './locales/ru.js';
+
+const init = async () => {
+  const defaultLanguage = 'ru';
+  const i18nextInstance = i18next.createInstance();
+  await i18nextInstance.init({
+    lng: defaultLanguage,
+    debug: true,
+    ru,
+  }).then(() => {
+    yup.setLocale({
+      mixed: {
+        valid: i18nextInstance.t('errors.validation.url'),
+        notOneOf: i18nextInstance.t('errors.validation.notUnique'),
+      },
+      string: {
+        url: i18nextInstance.t('errors.parseError'),
+      },
+    });
+    return i18nextInstance;
+  });
+};
 
 const validate = async (url, urls) => {
-  const schema = yup.string().required().url().notOneOf(urls);  
+  const schema = yup.string().required().url().notOneOf(urls);
   return schema.validate(url);
 };
 
-const app = () => {
-  const elements = {
-    form: document.querySelector('.rss-form'),
-    input: document.querySelector('#url-input'),
-    button: document.querySelector('button[type="submit"]'),
-  };
-
+const buildInitialState = () => {
   const state = {
     form: {
       valid: null,
       processState: 'filling',
       errors: [],
     },
+    feedback: null,
     posts: [],
     feeds: [],
   };
+  return state;
+};
 
-  //написать функция реднера во view.js
+const app = (i18nextInstance) => {
+  init();
+  const state = buildInitialState();
+
+  const elements = {
+    form: document.querySelector('.rss-form'),
+    input: document.querySelector('#url-input'),
+    button: document.querySelector('button[type="submit"]'),
+  };
+
+  // написать функция реднера во view.js
   const watchedState = onChange(state, (path, current, previous) => {
     render();
-  })
+  });
 
   // добавление слушателя на форму
-    elements.form.addEventListener('submit', (e) => {
+  elements.form.addEventListener('submit', (e) => {
     watchedState.elements.form.processState = 'processing';
     e.preventDefault();
     const formData = new FormData(e.target);
@@ -46,10 +77,8 @@ const app = () => {
         watchedState.form.valid = false;
         watchedState.form.processState = 'failed';
         watchedState.form.errors.push(error);
-
       });
   });
 };
 
- export default app();
- 
+export default app();
---
+++ src/locales/ru.js
@@ -0,0 +1,12 @@
+export default {
+  translation: {
+    success: 'RSS успешно загружен',
+    errors: {
+      validation: {
+        url: 'Ссылка должна быть валидным URL',
+        notUnique: 'RSS уже существует',
+      },
+      parseError: 'Ресурс не содержит валидный RSS',
+    },
+  },
+};
